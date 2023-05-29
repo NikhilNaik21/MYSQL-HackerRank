e25666b9748f892da59f28384904cbe80fec1433
NikhilNaik <111115551+NikhilNaik21@users.noreply.github.com>
2023-05-29 22:02:52 +0530

    Update Solutions.sql

--- RoadtoFirstStar/Solutions.sql
+++ RoadtoFirstStar/Solutions.sql
@@ -23,3 +23,33 @@ select City,state from station;
 -- Weather Observation Station 3
 select distinct city from station where (id%2=0);
 
+-- Weather Observation Station 4
+select count(city)-count(distinct city) from station;
+
+-- Weather Observation Station 5
+select city,length(city) from  station order by length(city),city limit 1;
+select city,length(city) from station  order by length(city) desc ,city limit 1;
+
+-- Weather Observation Station 6
+select distinct city from station where city like 'a%' or 
+city like "e%" or
+city like  "i%" or
+city like "o%" or
+city like "u%" ;
+
+-- Weather Observation Station 7
+select distinct city from station where city like '%a' or 
+city like "%e" or
+city like  "%i" or
+city like "%o" or
+city like "%u" ;
+
+
+-- Weather Observation Station 8
+select distinct city from station where city regexp '^[AEIOU].*[AEIOU]$';
+
+-- Weather Observation Station 9
+select distinct city from station where city not  regexp  '^[AEIOU]';
+
+-- Weather Observation Station 10
+select distinct city from station where city not  regexp '[AEIOU]$'
