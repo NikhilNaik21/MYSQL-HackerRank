e3b4a1772fbd9f38c0d68c7c5b376babcc14fa80
NikhilNaik <111115551+NikhilNaik21@users.noreply.github.com>
2023-07-19 21:10:45 +0530

    Update and rename DrawTheTriangle1.sql to DrawTheTriangle2.sql

--- MyWorkspace/DrawTheTriangle2.sql
+++ MyWorkspace/DrawTheTriangle2.sql
@@ -1,10 +1,15 @@
+
 DELIMITER $$
 
 CREATE PROCEDURE stars(r INT) 
     BEGIN 
+    declare cnt int;
+    set cnt=1;
         WHILE r >= 1 DO 
-            SELECT repeat('* ', r);
+        
+            SELECT repeat('* ',cnt);
             SET r = r - 1;
+            set cnt = cnt + 1;
             END WHILE;
     END; $$
     
