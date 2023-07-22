2432cf6c89ba9c3a9c4471438c2ca30b8a8e14d8
NikhilNaik <111115551+NikhilNaik21@users.noreply.github.com>
2023-07-22 15:37:07 +0530

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -1,5 +1,7 @@
 --Weather observation station 11
- select distinct city from station where city not regexp '^[aeiou]' or city not regexp '[aeiou]$';
+ ``` 
+select distinct city from station where city not regexp '^[aeiou]' or city not regexp '[aeiou]$'; 
+```
  
 --Weather observation station 12
 select distinct city from station where  city not regexp '^[aeiou]' and city not regexp '[aeiou]$';
@@ -11,7 +13,8 @@ select round(sum(LAT_N),4)from station where LAT_N>38.7880 and LAT_N < 137.2345;
 select round(max(LAT_N),4) from station where LAT_N<137.2345;
 
 --Weather observation station 15
-select round(long_w, 4) from station 
+select round(long_w, 4) 
+ from station 
 where lat_n = (select max(lat_n) 
 from station where lat_n < 137.2345);
 
